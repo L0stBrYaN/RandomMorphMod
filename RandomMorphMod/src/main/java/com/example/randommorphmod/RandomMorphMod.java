5e03987dc04698ead3738ae913990079802c0145
L0stBrYaN <54414834+L0stBrYaN@users.noreply.github.com>
2025-06-05 21:24:17 +0200

    Add files via upload

---
+++ RandomMorphMod/src/main/java/com/example/randommorphmod/RandomMorphMod.java
@@ -0,0 +1,10 @@
+package com.example.randommorphmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RandomMorphMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Random Morph Mod initialized!");
+    }
+}
---
+++ RandomMorphMod/src/main/java/com/example/randommorphmod/RandomMorphModClient.java
@@ -0,0 +1,10 @@
+package com.example.randommorphmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RandomMorphModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("Random Morph Mod client initialized!");
+    }
+}
